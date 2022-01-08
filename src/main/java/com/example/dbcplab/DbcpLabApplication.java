e08fcb5eba025304f80308fc81c90855948ea1f5
wonsangL <harryandws@gmail.com>
2022-01-08 17:37:47 +0900

    Project Init

---
+++ src/main/java/com/example/dbcplab/DbcpLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbcplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcpLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbcpLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbcplab/DbcpLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbcplab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcpLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
